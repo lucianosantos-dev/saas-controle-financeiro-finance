b3430d095dc8a19b7d995eb5e5429c856ce562ef
Luciano Santos de Jesus Reis <lucianodev.contato@gmail.com>
2026-08-07 18:35:00 -0300

    primeiro commit

---
+++ src/main/java/com/lucianodev/saascontrolefinanceirofinance/SaascontrolefinanceirofinanceApplication.java
@@ -0,0 +1,13 @@
+package com.lucianodev.saascontrolefinanceirofinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaascontrolefinanceirofinanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaascontrolefinanceirofinanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucianodev/saascontrolefinanceirofinance/SaascontrolefinanceirofinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucianodev.saascontrolefinanceirofinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaascontrolefinanceirofinanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
