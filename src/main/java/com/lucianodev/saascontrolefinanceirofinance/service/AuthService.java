8f1b7c880e424b7eb82e8c03988b03f4d1059a73
Luciano Santos de Jesus Reis <lucianodev.contato@gmail.com>
2026-08-21 11:58:21 -0300

    feat: implementa métodos para o fluxo de redefinição de senha

--- src/main/java/com/lucianodev/saascontrolefinanceirofinance/service/AuthService.java
+++ src/main/java/com/lucianodev/saascontrolefinanceirofinance/service/AuthService.java
@@ -1,9 +1,12 @@
 package com.lucianodev.saascontrolefinanceirofinance.service;
 
+import com.lucianodev.saascontrolefinanceirofinance.dto.request.EmailRequest;
 import com.lucianodev.saascontrolefinanceirofinance.dto.request.LoginRequest;
+import com.lucianodev.saascontrolefinanceirofinance.dto.request.NovaSenhaRequest;
 import com.lucianodev.saascontrolefinanceirofinance.dto.response.LoginResponse;
 import com.lucianodev.saascontrolefinanceirofinance.entity.TokenVerificacao;
 import com.lucianodev.saascontrolefinanceirofinance.entity.Usuario;
+import com.lucianodev.saascontrolefinanceirofinance.enums.TipoVerificacao;
 import com.lucianodev.saascontrolefinanceirofinance.exception.EmailNaoVerificadoException;
 import com.lucianodev.saascontrolefinanceirofinance.exception.UsuarioInativoException;
 import com.lucianodev.saascontrolefinanceirofinance.repository.UsuarioRepository;
@@ -27,14 +30,23 @@ public class AuthService {
     private final UsuarioRepository usuarioRepository;
     private final JwtEncoder jwtEncoder;
     private final PasswordEncoder passwordEncoder;
+    private final EmailService emailService;
 
 
-    public AuthService(UsuarioService usuarioService, TokenVerificacaoService tokenVerificacaoService, UsuarioRepository usuarioRepository, JwtEncoder jwtEncoder, PasswordEncoder passwordEncoder) {
+    public AuthService(UsuarioService usuarioService,
+                       TokenVerificacaoService tokenVerificacaoService,
+                       UsuarioRepository usuarioRepository,
+                       JwtEncoder jwtEncoder,
+                       PasswordEncoder passwordEncoder,
+                       EmailService emailService
+                       ) {
+
         this.usuarioService = usuarioService;
         this.tokenVerificacaoService = tokenVerificacaoService;
         this.usuarioRepository = usuarioRepository;
         this.jwtEncoder = jwtEncoder;
         this.passwordEncoder = passwordEncoder;
+        this.emailService = emailService;
     }
 
     @Transactional
@@ -42,6 +54,45 @@ public class AuthService {
         TokenVerificacao tokenValido = tokenVerificacaoService.validarToken(token);
         tokenVerificacaoService.consumirToken(tokenValido);
         usuarioService.marcarEmailComoVerificado(tokenValido.getUsuario());
+    }
+
+    @Transactional
+    public void redefinirSenha(NovaSenhaRequest request) {
+
+        TokenVerificacao tokenValido = tokenVerificacaoService.validarToken(request.token());
+
+        Usuario usuario = tokenValido.getUsuario();
+
+        usuario.setSenhaHash(passwordEncoder.encode(request.novaSenha()));
+
+        usuarioRepository.save(usuario);
+
+        tokenVerificacaoService.consumirToken(tokenValido);
+    }
+
+    @Transactional
+    public void solicitarRedefinicao(EmailRequest request) {
+        Usuario user = usuarioRepository.findByEmail(request.email())
+                .orElse(null);
+
+        if (user == null) return;
+
+        String token = tokenVerificacaoService.gerarToken(user, TipoVerificacao.REDEFINICAO_SENHA);
+
+        enviarEmailRedefinicaoSenha(user, token);
+    }
+
+    private void enviarEmailRedefinicaoSenha(Usuario usuario, String token) {
+        String destino = usuario.getEmail();
+        String assunto = "FINANCE Controle Financeiro: Redefinição de Senha";
+        String link = "http://localhost:4200/auth/redefinir-senha/?token=" + token;
+
+        String corpo = "Olá " + usuario.getNome() + "!\n\n"
+                + "Você solicitou a redefinição da sua senha.\n"
+                + "Clique no link abaixo para criar uma nova senha:\n"
+                + link;
+
+        emailService.sendEmail(destino, assunto, corpo);
     }
 
     @Transactional
